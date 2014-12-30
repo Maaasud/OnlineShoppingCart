4914f4dbf17277ae62296e6c91e81f199337a308
nabinaryal <np.nabin.aryal@gmail.com>
2014-12-30 11:42:09 -0800

    adding two class

---
+++ OnlineShoppingCart.BAL/Repository/Contact.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OnlineShoppingCart.BAL.Repository
+{
+    public class Contact
+    {
+        public int ContactID { get; set; }
+
+
+        public string Email { get; set; }
+        
+        public string Phone { get; set; }
+
+        public string Mobile { get; set; }
+
+        public string Website { get; set; }
+
+        public DateTime DateCreated { get; set; }
+
+        public DateTime DateModified { get; set; }
+
+        public DateTime DateDeleted { get; set; }
+
+    }
+}
