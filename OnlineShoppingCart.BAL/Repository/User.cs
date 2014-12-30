cd125aacef73d76e0b646e7d16581934b41383db
nabinaryal <np.nabin.aryal@gmail.com>
2014-12-30 05:43:09 -0800

    Adding Edmx to DAL and class to BAL and referencing dll

---
+++ OnlineShoppingCart.BAL/Repository/User.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OnlineShoppingCart.BAL.Repository
+{
+    public class User
+    {
+        public int UserID { get; set; }
+
+        public int FirstName { get; set; }
+
+        public int LastName { get; set; }
+
+        public int Password { get; set; }
+
+        public int ContactID { get; set; }
+
+        public int CreditCardID { get; set; }
+
+        public int AddressID { get; set; }
+
+        public int CompanyID { get; set; }
+
+        public int UserTypeID { get; set; }
+
+        public int UserRoleID { get; set; }
+
+        public int IsActive { get; set; }
+
+        public int DateCreated { get; set; }
+
+        public int DateModified { get; set; }
+
+        public int DateDeleted { get; set; }
+    }
+}
---
+++ OnlineShoppingCart/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OnlineShoppingCart.DAL;
+
+namespace OnlineShoppingCart.Controllers
+{
+    public class HomeController : Controller
+    {
+        OnlineShoppingCartDbContext _dbContext;
+        //
+        // GET: /Default/
+        public HomeController(OnlineShoppingCartDbContext context)
+        {
+            _dbContext = context;
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
