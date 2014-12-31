d542e4561176be2359e8c46bd13d9afa59885319
nabinaryal <np.nabin.aryal@gmail.com>
2014-12-31 03:06:18 -0800

    adding models in Bal

---
+++ OnlineShoppingCart.BAL/Repository/SaleRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OnlineShoppingCart.BAL.Repository
+{
+    public class SaleRepository
+    {
+		public int SalesID { get; set; }
+		
+        public int ProductID { get; set; }
+		
+        public int UserID { get; set; }
+		
+        public int UnitSold { get; set; }
+		
+        public int Amount { get; set; }
+		
+        public DateTime DateCreated { get; set; }
+		
+        public DateTime DateModified { get; set; }
+		
+        public DateTime DateDeleted { get; set; }
+    }
+}
---
+++ OnlineShoppingCart.BAL/Repository/UserRoleRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OnlineShoppingCart.BAL.Repository
+{
+    public class UserRoleRepository
+    {
+        public int UserRoleID { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime DateCreated { get; set; }
+
+        public DateTime DateModified { get; set; }
+
+        public DateTime DateDeleted { get; set; } 
+    }
+}
