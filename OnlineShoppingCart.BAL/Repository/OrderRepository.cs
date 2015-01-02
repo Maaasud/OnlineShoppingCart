2e58c6c26472e864ca5f07cdd4646aa1a2039319
nabinaryal <np.nabin.aryal@gmail.com>
2015-01-02 04:48:53 -0800

    Adding file into BAL

---
+++ OnlineShoppingCart.BAL/Repository/OrderRepository.cs
@@ -0,0 +1,28 @@
+﻿
+namespace OnlineShoppingCart.BAL.Repository
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+    using OnlineShoppingCart.DAL;
+
+    public class OrderRepository
+    {
+        public int OrderID { get; set; }
+
+        public int UserID { get; set; }
+
+        public Users User { get; set; }
+
+        public int TotalPrice { get; set; }
+
+        public bool IsCheckout { get; set; }
+
+        public DateTime DateCreated { get; set; }
+
+        public DateTime DateModified { get; set; }
+
+        public DateTime DateDeleted { get; set; }
+    }
+}
